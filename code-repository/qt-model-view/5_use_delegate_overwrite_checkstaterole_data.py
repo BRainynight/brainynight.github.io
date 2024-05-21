e340414f66ab982e865c492719c7337db2f15afc
Judy <judy1001kitten@gmail.com>
2024-05-21 10:13:19 +0800

    add python example: use delegate to remove checkbox even the checkStateRole

---
+++ code-repository/qt-model-view/5_use_delegate_overwrite_checkstaterole_data.py
@@ -0,0 +1,85 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QTableView, QCheckBox, QVBoxLayout, QWidget, QHBoxLayout
+from PyQt5.QtWidgets import QStyledItemDelegate
+from PyQt5.QtGui import QStandardItemModel
+from PyQt5.QtCore import Qt
+
+class CustomizeCheckBox(QWidget):
+    def __init__(self, parent, ftor ):
+        super().__init__(parent)
+        l = QHBoxLayout(self)
+        self.cb = QCheckBox(self)
+        l.addWidget(self.cb)
+        l.setAlignment(Qt.AlignCenter)
+        l.setContentsMargins(0, 0, 0, 0) # You must set margins, otherwise checkbox style is strange.
+        self.setLayout(l)
+        self.cb.stateChanged.connect(ftor)
+
+class NoCheckboxDelegate(QStyledItemDelegate):
+    def paint(self, painter, option, index):
+        # Skip painting the checkbox
+        if index.data(Qt.CheckStateRole) is not None:
+            value = index.data(Qt.DisplayRole)
+            if value is not None:
+                painter.save()
+                # painter.drawText(option.rect, Qt.AlignCenter, value)
+                painter.restore()
+        else:
+            super().paint(painter, option, index)
+
+
+class CheckBoxTableModel(QStandardItemModel):
+    def __init__(self, rows, columns, parent=None):
+        super().__init__(rows, columns, parent)
+
+    def setData(self, index, value, role=Qt.EditRole):
+        if index.isValid() and role in [ Qt.CheckStateRole, Qt.UserRole]:
+            if index.row() == 0:
+                self.setItemData(index, {Qt.CheckStateRole:value, Qt.UserRole:value}) # Don't set to CheckStateRole!! it causes problem!!
+            else:
+                self.setItemData(index, {Qt.UserRole:value})
+            # self.dataChanged.emit(index, index, [Qt.CheckStateRole]) # No need to emit again!
+            return True
+        return False
+
+class TableWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.model.dataChanged.connect(self.on_item_changed)
+
+    def initUI(self):
+        layout = QVBoxLayout(self)
+        self.tableView = QTableView(self)
+        self.delegate = NoCheckboxDelegate(self.tableView)
+        self.tableView.setItemDelegate(self.delegate)
+        self.model = CheckBoxTableModel(3, 4) 
+        self.tableView.setModel(self.model)
+
+        for row in range(self.model.rowCount()):
+            for column in range(self.model.columnCount()):
+                index = self.model.index(row, column)
+                callback = self.getCheckBoxHandler(index)
+                w = CustomizeCheckBox(self, callback)
+                self.tableView.setIndexWidget(index, w)
+
+        layout.addWidget(self.tableView)
+        self.setLayout(layout)
+        self.setGeometry(100, 100, 400, 200)
+        self.setWindowTitle('QTableView with QCheckBox')
+        self.show()
+    
+    def on_item_changed(self, item):
+        val  = self.model.data(item, Qt.UserRole)
+        print("Current data is,", val)
+       
+    def getCheckBoxHandler(self, index):
+        def handler(state):
+            self.model.setData(index, state , Qt.UserRole)
+        return handler
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWin = TableWidget()
+    sys.exit(app.exec_())
